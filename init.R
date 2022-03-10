4b557f42dd0f26338050aad3aefb037c48f4cf53
Steven Lio <stevenlio88@yahoo.com.hk>
2022-03-10 11:01:28 -0800

    Delete msleep

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'plotly', 'ggplot2', 'tidyverse'))
+install.packages(c('dash', 'dashCoreComponents', 'dashHtmlComponents', 'readr', 'here', 'ggthemes', 'remotes', 'plotly', 'ggplot2', 'tidyverse', 'dplyr'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
